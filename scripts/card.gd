3fe7a5e6d95e297033b8ec784aecbea7d3ea2f56
Mayur Wadhwani <mayurwadhwanid@gmail.com>
2025-06-14 15:02:09 +1200

    Removed Animation and shader, for future once the art style is established

--- scripts/card.gd
+++ scripts/card.gd
@@ -1,13 +1,27 @@
 extends Area2D
 
 @onready var animator: AnimationPlayer = $AnimationPlayer
+@onready var card: Sprite2D = $PlayingCards
+@onready var back: Sprite2D = $PlayingCardBacks
+@onready var collider: CollisionShape2D = $CollisionShape2D
 
 func _ready() -> void:
 	connect("mouse_entered", make_focussed)
 	connect("mouse_exited", leave_focussed)
 
+func _input(event: InputEvent) -> void:
+	if event is InputEventMouseButton:
+		card_opened()
+
 func make_focussed() -> void:
-	animator.play("hover")
+	# Focus Animation or Shader
+	pass
 
 func leave_focussed() -> void:
-	animator.play_backwards("hover")
+	# Reverse Focus Animation or Shader
+	pass
+
+func card_opened() -> void:
+	# Fade the card back
+	back.visible = false
+	collider.disabled = true
