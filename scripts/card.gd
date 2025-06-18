f832b0b4c093f17dc93da1472cecd921bb76aa52
Mayur Wadhwani <mayurwadhwanid@gmail.com>
2025-06-19 07:26:53 +1200

    Card reveal animation

--- scripts/card.gd
+++ scripts/card.gd
@@ -3,6 +3,8 @@ extends Area2D
 @onready var card: Sprite2D = $PlayingCards
 @onready var back: Sprite2D = $PlayingCardBacks
 @onready var collider: CollisionShape2D = $CollisionShape2D
+@onready var emitter: GPUParticles2D = $Emitter
+@onready var timer: Timer = $Timer
 
 var card_still_in_play = true
 
@@ -30,7 +32,26 @@ func _on_mouse_entered() -> void:
 func _on_mouse_exited() -> void:
 	change_focus(false)
 
-func set_card_complete() -> void:
-	card_still_in_play = false
+func reveal_card_with_animation(is_match: bool) -> void:
+	back.visible = false
 	disconnect("mouse_entered", _on_mouse_entered)
 	disconnect("mouse_exited", _on_mouse_exited)
+	disconnect("input_event", _on_input_event)
+	if is_match:
+		emitter.emitting = true
+		card_still_in_play = false
+		# points signal? Maybe the manager should do this
+	else:
+		# Incorrect match animation?
+		timer.start()
+
+
+func _on_timer_timeout() -> void:
+	back.visible = true
+	connect("mouse_entered", _on_mouse_entered)
+	connect("mouse_exited", _on_mouse_exited)
+	connect("input_event", _on_input_event)
+
+func _on_input_event(_viewport: Node, event: InputEvent, _shape_idx: int) -> void:
+	if event is InputEventMouseButton:
+		reveal_card_with_animation(true)
