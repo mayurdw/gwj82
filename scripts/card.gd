122e0f165d4d121fbc7ebde207992b41955613a0
Mayur Wadhwani <mayurwadhwanid@gmail.com>
2025-06-20 12:33:39 +1200

    Using Control Node

--- scripts/card.gd
+++ scripts/card.gd
@@ -1,42 +1,36 @@
-extends Area2D
+extends AspectRatioContainer
 
-@onready var card: Sprite2D = $PlayingCards
-@onready var back: Sprite2D = $PlayingCardBacks
-@onready var collider: CollisionShape2D = $CollisionShape2D
+#region: OnReadys
+@onready var card: TextureRect = $PlayingCards
+@onready var back: TextureRect = $PlayingCardBacks
 @onready var emitter: GPUParticles2D = $Emitter
 @onready var timer: Timer = $Timer
+#endregion
 
+#region: State Variable
 var card_still_in_play = true
-
 var cardInfo: CardInfo
+#endregion
 
 func _ready() -> void:
 	if cardInfo == null:
 		cardInfo = CardInfo.new()
-		cardInfo.card_sprite = CardInfo.CardSprite.FOUR_SPADE
-	card.frame = cardInfo.card_sprite
+	card.texture.region = cardInfo.get_card_region()
 
 func change_focus(is_focussed: bool) -> void:
 	back.material.set("shader_parameter/is_active", is_focussed)
 	create_tween().tween_method(_update_radius, 0.1, 2.0, 0.5)
 
-func _update_radius(radius: float) -> void:
-	back.material.set_shader_parameter("radius", radius)
-
-func update_radius(value: float):
-	back.material.set_shader_parameter("radius", value)
-
-func _on_mouse_entered() -> void:
-	change_focus(true)
-
-func _on_mouse_exited() -> void:
-	change_focus(false)
+func _on_gui_input(event: InputEvent) -> void:
+	if event is InputEventMouseButton:
+		reveal_card_with_animation(false)
 
 func reveal_card_with_animation(is_match: bool) -> void:
 	back.visible = false
 	disconnect("mouse_entered", _on_mouse_entered)
 	disconnect("mouse_exited", _on_mouse_exited)
-	disconnect("input_event", _on_input_event)
+	disconnect("gui_input", _on_gui_input)
+	
 	if is_match:
 		emitter.emitting = true
 		card_still_in_play = false
@@ -50,8 +44,24 @@ func _on_timer_timeout() -> void:
 	back.visible = true
 	connect("mouse_entered", _on_mouse_entered)
 	connect("mouse_exited", _on_mouse_exited)
-	connect("input_event", _on_input_event)
+	connect("gui_input", _on_gui_input)
 
-func _on_input_event(_viewport: Node, event: InputEvent, _shape_idx: int) -> void:
-	if event is InputEventMouseButton:
-		reveal_card_with_animation(true)
+func _update_radius(radius: float) -> void:
+	back.material.set_shader_parameter("radius", radius)
+
+func update_radius(value: float):
+	back.material.set_shader_parameter("radius", value)
+
+#region: Focus Signal Callbacks
+func _on_mouse_entered() -> void:
+	change_focus(true)
+
+func _on_mouse_exited() -> void:
+	change_focus(false)
+
+func _on_focus_entered() -> void:
+	change_focus(true)
+
+func _on_focus_exited() -> void:
+	change_focus(false)
+#endregion
--- scripts/resource_scripts/CardInfo.gd
+++ scripts/resource_scripts/CardInfo.gd
@@ -1,85 +1,37 @@
 class_name CardInfo
 extends RefCounted
 
-@export var card_sprite: CardSprite
+@export var cardNumber: CardNumber = CardNumber.ACE
+@export var cardFamily: CardFamily = CardFamily.CLUB
 
-##
-## If returns -1, throw error
-func get_card_number() -> int:
-	match card_sprite:
-		CardSprite.ACE_CLUB, CardSprite.ACE_DIAMOND, CardSprite.ACE_HEART, CardSprite.ACE_SPADE: return 1
-		CardSprite.TWO_DIAMOND, CardSprite.TWO_CLUB, CardSprite.TWO_HEART, CardSprite.TWO_SPADE: return 2
-		CardSprite.THREE_CLUB, CardSprite.THREE_DIAMOND, CardSprite.THREE_HEART, CardSprite.THREE_SPADE: return 3
-		CardSprite.FOUR_CLUB, CardSprite.FOUR_DIAMOND, CardSprite.FOUR_HEART, CardSprite.FOUR_SPADE: return 4
-		CardSprite.FIVE_CLUB, CardSprite.FIVE_DIAMOND, CardSprite.FIVE_HEART, CardSprite.FIVE_SPADE: return 5
-		CardSprite.SIX_CLUB, CardSprite.SIX_DIAMOND, CardSprite.SIX_HEART, CardSprite.SIX_SPADE: return 6
-		CardSprite.SEVEN_CLUB, CardSprite.SEVEN_DIAMOND, CardSprite.SEVEN_HEART, CardSprite.SEVEN_SPADE: return 7
-		CardSprite.EIGHT_CLUB, CardSprite.EIGHT_DIAMOND, CardSprite.EIGHT_HEART, CardSprite.EIGHT_SPADE: return 8
-		CardSprite.NINE_CLUB, CardSprite.NINE_DIAMOND, CardSprite.NINE_HEART, CardSprite.NINE_SPADE: return 9
-		CardSprite.TEN_CLUB, CardSprite.TEN_DIAMOND, CardSprite.TEN_HEART, CardSprite.TEN_SPADE: return 10
-		CardSprite.JACK_CLUB, CardSprite.JACK_DIAMOND, CardSprite.JACK_HEART, CardSprite.JACK_SPADE: return 11
-		CardSprite.QUEEN_CLUB, CardSprite.QUEEN_DIAMOND, CardSprite.QUEEN_HEART, CardSprite.QUEEN_SPADE: return 12
-		CardSprite.KING_CLUB, CardSprite.KING_DIAMOND, CardSprite.KING_HEART, CardSprite.KING_SPADE: return 13
-		_: return -1
-
-func is_complete_match(cardInfo: CardInfo) -> bool:
-	return cardInfo.card_sprite == card_sprite
+func is_match(cardInfo: CardInfo) -> bool:
+	return cardFamily == cardInfo.cardFamily and is_partial_match(cardInfo) 
 
 func is_partial_match(cardInfo: CardInfo) -> bool:
-	return cardInfo.get_card_number() == get_card_number()
+	return cardNumber == cardInfo.cardNumber
 
-# This is arranged in the order they are in the spreadsheet
-enum CardSprite {
-	QUEEN_CLUB = 0,
-	FOUR_CLUB,
-	EIGHT_HEART,
-	ACE_DIAMOND,
-	QUEEN_SPADE,
-	FOUR_SPADE,
-	KING_CLUB,
-	THREE_CLUB,
-	SEVEN_HEART,
-	TEN_DIAMOND,
-	KING_SPADE,
-	THREE_SPADE,
-	JACK_CLUB,
-	TWO_CLUB,
-	SIX_HEART,
-	NINE_DIAMOND,
-	JACK_SPADE,
-	TWO_HEART,
-	ACE_CLUB,
-	FIVE_HEART = 20,
-	EIGHT_DIAMOND,
-	ACE_SPADE,
-	TEN_CLUB = 24,
-	QUEEN_HEART,
-	FOUR_HEART,
-	SEVEN_DIAMOND,
-	TEN_SPADE,
-	NINE_CLUB = 30,
-	KING_HEART,
-	THREE_HEART,
-	SIX_DIAMOND,
-	NINE_SPADE,
-	EIGHT_CLUB = 36,
-	JACK_HEART,
-	TWO_SPADE,
-	FIVE_DIAMOND,
-	EIGHT_SPADE,
-	SEVEN_CLUB = 42,
-	ACE_HEART,
-	QUEEN_DIAMOND,
-	FOUR_DIAMOND,
-	SEVEN_SPADE,
-	SIX_CLUB = 48,
-	TEN_HEART,
-	KING_DIAMOND,
-	THREE_DIAMOND,
-	SIX_SPADE,
-	FIVE_CLUB = 54,
-	NINE_HEART,
-	JACK_DIAMOND,
-	TWO_DIAMOND,
-	FIVE_SPADE
+func get_card_region() -> Rect2:
+	return Rect2(cardNumber * 150, cardFamily * 200, 140.0, 190.0)
+
+enum CardNumber {
+	ACE = 0,
+	TWO,
+	THREE,
+	FOUR,
+	FIVE,
+	SIX,
+	SEVEN,
+	EIGHT,
+	NINE,
+	TEN,
+	JACK,
+	QUEEN,
+	KING
+}
+
+enum CardFamily {
+	CLUB = 0,
+	SPADE,
+	DIAMOND,
+	HEART
 }
